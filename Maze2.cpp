1a505f45c30f2978fd2500f4d4ad48a9474588e4
DIM1873111 <2991527695@qq.com>
2026-06-11 00:42:36 +0800

    框架2制作进度
    添加了json类 (类里面加类搞了我三天三夜才弄明白(哭(bush

---
+++ Maze2.cpp
@@ -0,0 +1,371 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <limits>  // numeric_limits
+#include <sstream>
+#include <memory>
+#include <algorithm>  // shuffle
+#include <random>
+#include <functional>
+#include <unordered_map>
+#include <chrono>
+#include <fstream>
+#include <filesystem>
+
+#include "Maze2.h"//Maze2.h头文件
+#include "json.hpp"//json库
+
+
+
+
+//__________________________________Config_Reading__________________________________
+
+
+bool Mazesimulate::Config_Reading::Check_config_file(){//检查配置文件是否存在
+return std::filesystem::exists(configPath);//检查配置文件是否存在
+}
+
+void Mazesimulate::Config_Reading::Add_config(std::string config_name, int* Address){
+config.push_back(config_Address{config_name, Address});//添加配置信息
+}
+
+
+
+
+void Mazesimulate::Config_Reading::Load_Config(){
+
+
+if(Check_config_file()){//如果配置文件存在
+
+Read_config();//读取配置文件并解析配置信息
+
+}else{//如果配置文件不存在
+
+Create_Config();//创建配置文件并写入默认配置
+
+}
+
+
+
+
+}
+
+void Mazesimulate::Config_Reading::Create_Config(){//创建json文件 并且写入默认配置
+
+if(Check_config_file()){//如果配置文件存在
+mazesimulate->Log_class.cout_Log("The config file already exists - > Trying to delete the file...", Log_Exit::Log_type::NOTICE_ENUM);//输出通知日志
+    if(std::remove(configPath.c_str())){//删除失败
+    
+        mazesimulate->Log_class.Exit("Failed to try to delete the file", Log_Exit::Exit_type::SYSTEM_ENUM);//输出错误调用退出
+    
+    }else{
+    
+        mazesimulate->Log_class.cout_Log("Deleted " + configPath + " files", Log_Exit::Log_type::NOTICE_ENUM);//输出通知日志
+    
+    }
+}
+
+//创建json文件
+
+if(config.size()){//检查配置表是否为空
+
+nlohmann::json configJson;
+
+    for(const auto& item : config){//遍历配置表
+        if(item.Address != nullptr) {   //检查指针是否为空
+            configJson[item.config_name] = *(item.Address);
+        }else{
+            configJson[item.config_name] = 0;  //默认处理
+        }
+    }
+
+
+    std::ofstream out(configPath);
+
+
+    if (out.is_open()) {
+        out << configJson.dump(4);  // 带缩进的格式化输出
+        out.close();
+
+        mazesimulate->Log_class.cout_Log("Created " + configPath + " files", Log_Exit::Log_type::NOTICE_ENUM);//输出通知日志
+
+
+    } else {
+
+
+        mazesimulate->Log_class.Exit("Failed to create the file", Log_Exit::Exit_type::SYSTEM_ENUM);//输出错误日志
+
+        
+    }
+
+}else{
+
+mazesimulate->Log_class.cout_Log("No matching table found. - > Cancel creating JSON", Log_Exit::Log_type::NOTICE_ENUM);//输出通知日志
+
+}
+
+}
+
+void Mazesimulate::Config_Reading::Read_config(){//读取json文件并解析配置信息
+
+std::ifstream inFile(configPath);//打开json文件
+if(inFile.is_open()){
+
+    nlohmann::json configJson;
+    inFile >> configJson;   // 解析 JSON
+    inFile.close();
+
+
+    for(const auto& item : config){
+    if(configJson.contains(item.config_name)){
+        int newValue = configJson[item.config_name].get<int>();
+        *(item.Address) = newValue;   //更新原变量
+        }
+    }
+
+
+
+}else{
+    mazesimulate->Log_class.cout_Log("Unable to open file " + configPath + " - > Launch using default configuration" , Log_Exit::Log_type::WARNING_ENUM);
+}
+
+
+}
+
+
+//__________________________________Log_Exit__________________________________
+
+
+
+void Mazesimulate::Log_Exit::cout_Log(std::string log, Log_type type){//输出日志信息并记录日志信息
+
+std::stringstream Temporary_log;//临时存储日志信息
+
+switch(type){
+
+    case Log_type::PROMPT_ENUM://提示日志
+        Temporary_log << "[\033[1mPROMPT\033[0m]" << log << std::endl;
+    break;
+
+    case Log_type::NOTICE_ENUM://通知日志
+        Temporary_log << "[\033[34mNOTICE\033[0m]" << log << std::endl;
+    break;
+
+    case Log_type::WARNING_ENUM://警告日志
+        Temporary_log << "[\033[33mWARNING\033[0m]" << log << std::endl;
+    break;
+
+    case Log_type::ERROR_ENUM://错误日志
+        Temporary_log << "[\033[31mERROR\033[0m]" << log << std::endl;
+    break;
+
+}
+//NOTICE
+std::cout << Temporary_log.str();//输出日志信息
+//记录日志信息(queue)
+historical_logs.push(Temporary_log.str());
+
+if(historical_logs.size() > (size_t)mazesimulate->Max_number_logs){//如果日志容器大小超过最大日志数
+
+    historical_logs.pop();//删除最早的日志信息
+
+}
+
+}
+
+
+
+void Mazesimulate::Log_Exit::Exit(std::string exit_log, Exit_type type){//输出错误信息并退出程序
+
+    std::stringstream Logout_Information;//临时存储错误信息
+
+
+switch(type){
+
+    case Exit_type::STANDARD_ENUM://标准退出
+        Logout_Information << "[\033[2mEXIT\033[0m]" << exit_log << std::endl;
+        break;
+
+    case Exit_type::OPERATIONAL_ENUM://退出程序
+        Logout_Information << "[\033[33mEXIT_OPERATIONAL\033[0m]" << exit_log << std::endl;
+        break;
+
+    case Exit_type::SYSTEM_ENUM://系统退出
+        Logout_Information << "[\033[90mEXIT_SYSTEM\033[0m]" << exit_log << std::endl;
+        break;
+
+
+    case Exit_type::ERROR_ENUM://错误退出
+        Logout_Information << "[\033[31mEXIT_ERROR\033[0m]" << exit_log << std::endl;
+        //输出退出前的错误信息
+        
+        if(historical_logs.empty()){break;}//如果日志容器为空跳过
+        std::cout << "[\033[38;5;214mERROR_LOG\033[0m]Output the last'" << historical_logs.size() << "'logs before exit" << std::endl; //输出最后的错误日志
+        
+        while(!historical_logs.empty()){
+            std::cout << historical_logs.front();
+            historical_logs.pop();
+            }
+        
+}
+
+
+
+
+std::cout << Logout_Information.str();//输出错误信息
+
+exit(0);//退出程序
+
+}
+
+
+
+//__________________________________Initialize__________________________________
+
+void Mazesimulate::Input_processing::Get_Input(){
+
+    std::vector<std::string> inputs; //临时存储输入的容器
+    std::string token; //临时存储每个字符串
+
+            std::cout << "Enter parameters to start the simulation" << std::endl;
+            std::cout << "[type '-help' to see available parameters]" << std::endl;
+            std::cout << ">";
+
+        while(std::cin >> token){
+
+
+
+
+
+            if(token == "-g"){//如果输入是-g代表开始(结束循环输入)
+                break;
+            }
+
+            inputs.push_back(token);
+
+            if(token == "-help"){//如果输入是-help代表显示帮助信息
+                std::cout << "Available parameters(Put at the back):" << std::endl;
+                std::cout << "-g: Start the simulation" << std::endl;
+                std::cout << "-help: Display available parameters" << std::endl;
+                std::cout << "-exit: Exit the simulation" << std::endl;
+                //配置列表
+                std::cout << "Config Parameter Table:" << std::endl;
+                    if(config.empty()){
+
+                        std::cout << "No configuration parameters available." << std::endl;
+
+                    }else{
+
+                        for(const auto& config : config){
+                        std::cout << config.config_name << ":" << config.config_description << " - > " << *config.Address <<std::endl;
+                        }
+
+                    }
+                std::cout << ">" ;
+                //mazesimulate->Log_class.Exit("Help displayed", Log_Exit::Exit_type::STANDARD_ENUM);
+                //break;
+            }
+
+            if(token == "-exit"){//如果输入是-exit代表退出(结束循环输入)
+                mazesimulate->Log_class.Exit("Simulation exited", Log_Exit::Exit_type::OPERATIONAL_ENUM);
+                break;
+            }
+
+
+        }
+
+
+    for(const auto& input : inputs){//处理输入内容
+        std::string strPart;
+        int numPart = 0;//默认值为0
+
+
+        size_t pos = input.find_first_of("0123456789");//找到第一个数字的位置
+        if(pos != std::string::npos) {
+            strPart = input.substr(0, pos);  // 数字前的字符串
+            numPart = std::stoi(input.substr(pos));  // 数字部分
+        }
+
+        UserInput.push_back(User_Input{strPart, numPart});//将输入内容存入UserInput容器
+        //调用日志
+        std::stringstream Temporary_log;
+        Temporary_log << "User input parameters: " << strPart << "," << numPart;
+    
+        mazesimulate->Log_class.cout_Log(Temporary_log.str(), Log_Exit::Log_type::PROMPT_ENUM);//输出提示日志
+    }
+
+    Matching_config_table();//匹配配置表
+
+}
+
+//add_config_table
+void Mazesimulate::Input_processing::add_config_table(std::string config_name, std::string config_description, int* Address){
+    config.push_back(config_Address{config_name, config_description, Address});//添加配置信息
+}
+
+
+//Matching_config_table
+void Mazesimulate::Input_processing::Matching_config_table(){
+int Invalid_input = 0;
+for(const auto& UserInput : UserInput){
+
+    for(const auto& config : config){
+        bool match = true;
+        if(UserInput.name == config.config_name){
+            *config.Address = UserInput.value;//匹配配置信息并赋值
+            match = false;
+            break;
+        }
+        if(match){//统计无效输入
+            Invalid_input++;
+        }
+    }
+
+}
+
+if(Invalid_input > 0){
+//输出日志
+std::stringstream Temporary_log;
+Temporary_log << "Invalid input parameters: " << Invalid_input << std::endl;
+mazesimulate->Log_class.cout_Log(Temporary_log.str(), Log_Exit::Log_type::WARNING_ENUM);//输出错误日志
+
+}
+
+
+
+}
+
+
+//__________________________________Data_Processing__________________________________
+
+void Mazesimulate::Data_Processing::Initialize_data(){//初始化数据
+
+
+//测试 
+mazesimulate->Input_class.add_config_table("mapx", "Map X length", &mazesimulate->Map_length_X);//添加配置信息
+mazesimulate->Input_class.add_config_table("mapy", "Map Y length", &mazesimulate->Map_length_Y);//添加配置信息
+mazesimulate->Config_class.Add_config("Map_length_X", &mazesimulate->Map_length_X);//添加配置信息
+mazesimulate->Config_class.Add_config("Map_length_Y", &mazesimulate->Map_length_Y);//添加配置信息
+
+mazesimulate->Config_class.Load_Config();//加载配置文件
+
+mazesimulate->Input_class.Get_Input();//调用用户输入
+
+std::cout << mazesimulate->Map_length_X << " " << mazesimulate->Map_length_Y << std::endl; //尝试输出地图长度
+
+
+
+
+//mazesimulate->Log_class.Exit("Initialization completed", Log_Exit::Exit_type::ERROR_ENUM);
+}
+
+//__________________________________Mazesimulate__________________________________
+
+
+void Mazesimulate::Framework_Entry(){//初始化
+
+
+Data_class.Initialize_data();//初始化数据
+
+
+}
--- Maze2.h
+++ Maze2.h
@@ -6,130 +6,189 @@
 #include <string>
 #include <functional>
 #include <unordered_map>
+#include <queue>
 
 
-class Mazesimulate{
+#include "json.hpp"//包含json库
 
-    private://私有
-
-        int Max_log = 20;//Log_Exit类最大日志数
-
-        //key
-        friend class Input_processing;//用户输入处理类
-        friend class Data_Processing;//数据处理类
-        friend class Log_Exit;//日志或退出类
+//Mazesimulate作为Maze_AI的第2版 这一版本打算重构框架结构 并且添加一些新的功能
+//原项目(老版本)完整项目链接:https://github.com/DIM1873111/Maze-Simulation-Test-Framework-
+//maze2开始日期2026年6月7日
+//迷宫模拟平台框架(.HPP)
 
 
 
-    
-        class Log_Exit{//日志或退出
+//[Maze2.h框架]
 
-            private://私有成员
+class Mazesimulate {
+    private:
 
-                Mazesimulate *mazesimulate;//获取Mazekye对象
+        int Max_number_logs = 20;//最大日志数
+        int Map_length_X = 10;//地图长度X
+        int Map_length_Y = 10;//地图长度Y
 
-                std::vector<std::string> Log_container;//日志容器
 
-            public://公有成员
 
-                enum class Log_type{//日志类型
+
+        //前向声明嵌套类
+        class Log_Exit;
+        class Input_processing;
+        class Data_Processing;
+        class Config_Reading;
+
+
+
+        class Config_Reading{//默认配置读取类(json文件读取)
+
+            private://私有
+
+                Mazesimulate* mazesimulate;
                 
-                PROMPT_ENUM,//提示日志
-                NOTICE_ENUM,//通知日志
-                WARNING_ENUM,//警告日志
-                ERROR_ENUM,//错误日志
-                
-                };            
-
-                enum class Exit_type{//退出类型
-
-                STANDARD_ENUM,//标准退出
-                OPERATIONAL_ENUM,//操作退出
-                ERROR_ENUM,//错误退出
-
+                struct config_Address{//配置表
+                    std::string config_name;//配置名
+                    int* Address;//配置地址
                 };
-                void cout_Log(std::string log, Log_type type);//日志函数
+            
+                std::string configPath = "config.json";//配置文件名
+            
+                std::vector<config_Address> config;//配置表
+            
+                void Create_Config();//创建json文件 并且写入默认配置
+            
+                void Read_config();//从json文件读取配置
+                
+                bool Check_config_file();//检查配置文件是否存在
+            
+            public://公有
 
-                void Exit(std::string exit, Exit_type type);//退出函数
+                Config_Reading(Mazesimulate* p):mazesimulate(p){}//构造函数
 
-                std::vector<std::string> Output_historical_logs();//输出历史日志函数
+                void Add_config(std::string config_name, int* Address);//添加配置
+
+                void Load_Config();//加载配置
+
+            
         };
 
 
+        class Log_Exit{//日志或退出处理
 
-        Log_Exit Log_class;//创建日志或退出对象
 
+            private://私有
+
+                Mazesimulate* mazesimulate;//获取Mazesimulate对象
+                std::queue<std::string> historical_logs;//历史日志队列
+
+            public:
+
+                Log_Exit(Mazesimulate* p):mazesimulate(p){}//构造函数
+
+
+                enum class Log_type{//日志类型
+
+                   PROMPT_ENUM,//提示日志
+                   NOTICE_ENUM,//通知日志
+                   WARNING_ENUM,//警告日志
+                   ERROR_ENUM,//错误日志
+                                   
+                };
+
+                enum class Exit_type{//退出类型
+
+                   STANDARD_ENUM,//标准退出
+                   OPERATIONAL_ENUM,//操作退出
+                   ERROR_ENUM,//错误退出
+                   SYSTEM_ENUM,//系统退出
+                };
+
+
+
+                void cout_Log(std::string log, Log_type type);//日志函数
+                void Exit(std::string exit, Exit_type type);//退出函数
+
+        };
 
 
         class Input_processing{//用户输入处理
 
-            private://私有成员
+            private://私有
 
-            //结构体
-            struct config_Address{
-            std::string config_name;//配置名
-            int* Address;//配置地址
-            };
+                Mazesimulate* mazesimulate;//获取mazesimulate对象
+                
 
-            struct User_Input{
-            std::string name;//用户输入名
-            int value;//用户输入值
-            };
+                //结构体
+                struct config_Address{
+                std::string config_name = "null";//配置名
+                std::string config_description = "null";//配置介绍
+                int* Address;//配置地址
+                };
 
-
-            std::vector<config_Address> config;//配置信息
-            std::vector<User_Input> UserInput;//用户输入信息
-
-            Log_Exit *Log_class;//获取Log_class对象
-            Mazesimulate *mazesimulate;//获取Mazekye对象
-
-            public://公有成员
+                struct User_Input{
+                std::string name;//用户输入配置名
+                int value;//用户输入值
+                };
 
 
+                std::vector<config_Address> config;//配置信息
+                std::vector<User_Input> UserInput;//用户输入信息
 
-            void Get_Input();//用户输入处理函数
+                void Matching_config_table();//匹配配置表
 
+            public:
+
+
+                Input_processing(Mazesimulate* p):mazesimulate(p){}
+
+
+                void add_config_table(std::string config_name, std::string config_description, int* Address);//添加配置表
+
+                void Get_Input();//获取用户输入
+
+                //void Parameter_Start();//参数启动
 
         };
-
-
-
 
         class Data_Processing{//数据处理
 
-            private://私有成员
 
-            Input_processing Input_class;//创建用户输入处理对象
+            private:
 
-            public://公有成员
 
-            void Initialize_data();//数据处理函数
+                Mazesimulate* mazesimulate;//获取mazesimulate对象
+
+
+            public:
+
+
+                Data_Processing(Mazesimulate* p):mazesimulate(p){}
+
+
+                void Initialize_data();//初始化数据
+
 
         };
 
 
 
 
+        //成员对象构造时传入 this
 
+        Config_Reading Config_class{this};//配置读取对象
+        Log_Exit Log_class{this};//日志处理对象
+        Input_processing Input_class{this};//用户输入处理对象
+        Data_Processing Data_class{this};//数据处理对象
 
 
 
     public://公有
 
-
-
-        void Framework_Entry();//入口
-
+        void Framework_Entry();//程序入口函数
 
 
 
 
-
-
-
-        //Mazesimulate();
-        //~Mazesimulate();
 };
+
 
 
 #endif // MAZEDIM_H
